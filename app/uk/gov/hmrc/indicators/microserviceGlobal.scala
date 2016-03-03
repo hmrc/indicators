cc1b0d201df8006a96b15802981fe783d75d346b
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2016-03-03 15:31:50 +0000

    PLATOPS-140 [PS] Added internal service version of microservice.global

--- app/uk/gov/hmrc/indicators/microserviceGlobal.scala
+++ app/uk/gov/hmrc/indicators/microserviceGlobal.scala
@@ -1,14 +1,17 @@
 package uk.gov.hmrc.indicators
 
+import com.kenshoo.play.metrics.MetricsFilter
 import com.typesafe.config.Config
-import play.api.{Application, Configuration, Play}
-import uk.gov.hmrc.play.audit.filters.AuditFilter
-import uk.gov.hmrc.play.auth.controllers.AuthParamsControllerConfig
-import uk.gov.hmrc.play.config.{AppName, ControllerConfig, RunMode}
-import uk.gov.hmrc.play.http.logging.filters.LoggingFilter
-import uk.gov.hmrc.play.microservice.bootstrap.DefaultMicroserviceGlobal
-import uk.gov.hmrc.play.auth.microservice.filters.AuthorisationFilter
 import net.ceedubs.ficus.Ficus._
+import play.api._
+import play.api.mvc.{EssentialAction, EssentialFilter, Filters}
+import uk.gov.hmrc.play.auth.controllers.AuthParamsControllerConfig
+import uk.gov.hmrc.play.config.{ControllerConfig, RunMode}
+import uk.gov.hmrc.play.filters.{NoCacheFilter, RecoveryFilter}
+import uk.gov.hmrc.play.graphite.GraphiteConfig
+import uk.gov.hmrc.play.http.logging.filters.LoggingFilter
+import uk.gov.hmrc.play.microservice.bootstrap.JsonErrorHandling
+import uk.gov.hmrc.play.microservice.bootstrap.Routing.RemovingOfTrailingSlashes
 
 
 object ControllerConfiguration extends ControllerConfig {
@@ -19,29 +22,48 @@ object AuthParamsControllerConfiguration extends AuthParamsControllerConfig {
   lazy val controllerConfigs = ControllerConfiguration.controllerConfigs
 }
 
-object MicroserviceAuditFilter extends AuditFilter with AppName {
-  override val auditConnector = MicroserviceAuditConnector
-  override def controllerNeedsAuditing(controllerName: String) = ControllerConfiguration.paramsForController(controllerName).needsAuditing
-}
-
 object MicroserviceLoggingFilter extends LoggingFilter {
   override def controllerNeedsLogging(controllerName: String) = ControllerConfiguration.paramsForController(controllerName).needsLogging
 }
 
-object MicroserviceAuthFilter extends AuthorisationFilter {
-  override lazy val authParamsConfig = AuthParamsControllerConfiguration
-  override lazy val authConnector = MicroserviceAuthConnector
-  override def controllerNeedsAuth(controllerName: String): Boolean = ControllerConfiguration.paramsForController(controllerName).needsAuth
+trait MicroserviceFilters {
+
+  def loggingFilter: LoggingFilter
+
+  def metricsFilter: MetricsFilter = MetricsFilter
+
+  protected lazy val defaultMicroserviceFilters: Seq[EssentialFilter] = Seq(
+    Some(metricsFilter),
+    Some(loggingFilter),
+    Some(NoCacheFilter),
+    Some(RecoveryFilter)).flatten
+
+  def microserviceFilters: Seq[EssentialFilter] = defaultMicroserviceFilters
 }
 
-object MicroserviceGlobal extends DefaultMicroserviceGlobal with RunMode {
-  override val auditConnector = MicroserviceAuditConnector
+object LoggingFilter extends LoggingFilter {
+  override def controllerNeedsLogging(controllerName: String) = ControllerConfiguration.paramsForController(controllerName).needsLogging
+}
 
-  override def microserviceMetricsConfig(implicit app: Application): Option[Configuration] = app.configuration.getConfig(s"microservice.metrics")
+object MicroserviceGlobal extends GlobalSettings
+  with RunMode
+  with GraphiteConfig
+  with RemovingOfTrailingSlashes
+  with JsonErrorHandling
+  with MicroserviceFilters {
 
-  override val loggingFilter = MicroserviceLoggingFilter
+  override def microserviceMetricsConfig(implicit app: Application): Option[Configuration] = app.configuration.getConfig("microservice.metrics")
 
-  override val microserviceAuditFilter = MicroserviceAuditFilter
+  lazy val appName = Play.current.configuration.getString("appName").getOrElse("APP NAME NOT SET")
 
-  override val authFilter = Some(MicroserviceAuthFilter)
+  override def onStart(app: Application) {
+    Logger.info(s"Starting microservice : $appName : in mode : ${app.mode}")
+    super.onStart(app)
+  }
+
+  override def doFilter(a: EssentialAction): EssentialAction = {
+    Filters(super.doFilter(a), microserviceFilters: _*)
+  }
+
+  override def loggingFilter: LoggingFilter = LoggingFilter
 }
