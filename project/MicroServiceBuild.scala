971297d783417b3de2bd8b1ae97b0c0216efd3ba
ashu.singh <asingh@equalexperts.com>
2016-05-10 08:54:01 +0100

    PLATOPS-140 fixed failing build due to unresolved git-client dependency

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -35,7 +35,7 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "play-url-binders" % playUrlBindersVersion,
     "uk.gov.hmrc" %% "play-config" % playConfigVersion,
     "uk.gov.hmrc" %% "play-json-logger" % playJsonLoggerVersion,
-    "uk.gov.hmrc" %% "git-client" % "0.2.0-4-g5d2f279",
+    "uk.gov.hmrc" %% "git-client" % "0.3.0",
     "uk.gov.hmrc" %% "domain" % domainVersion
   )
 
