fe5e347bcf6b85b37136436768b1045ddc8af2f0
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2017-02-14 11:37:12 +0000

    [PS] Updated license headers for 2017

--- app/uk/gov/hmrc/indicators/microserviceGlobal.scala
+++ app/uk/gov/hmrc/indicators/microserviceGlobal.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -69,5 +69,3 @@ object MicroserviceGlobal extends DefaultMicroserviceGlobal with RunMode {
 
   override val authFilter = None
 }
-
-
