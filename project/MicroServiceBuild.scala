c022c58926005a8b07fa24d00ed4bf3a1e3048f9
Dario Abdulrehman <dario.rehman@gmail.com>
2018-04-25 10:12:26 +0100

    PLATOPS-1347: Bump library dependencies

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -24,17 +24,17 @@ private object AppDependencies {
 
   private val microserviceBootstrapVersion = "6.7.0"
   private val playUrlBindersVersion        = "2.1.0"
-  private val domainVersion                = "4.1.0"
-  private val hmrcTestVersion              = "2.3.0"
+  private val domainVersion                = "5.1.0"
+  private val hmrcTestVersion              = "3.0.0"
 
   resolvers += "Sonatype OSS Snapshots" at "http://jcenter.bintray.com"
 
   val compile = Seq(
-    "uk.gov.hmrc"      %% "bootstrap-play-25" % "0.10.0",
+    "uk.gov.hmrc"      %% "bootstrap-play-25" % "1.5.0",
     "uk.gov.hmrc"      %% "play-url-binders"  % playUrlBindersVersion,
     "uk.gov.hmrc"      %% "domain"            % domainVersion,
     "uk.gov.hmrc"      %% "git-client"        % "0.6.0",
-    "uk.gov.hmrc"      %% "github-client"     % "1.14.0",
+    "uk.gov.hmrc"      %% "github-client"     % "1.21.0",
     "uk.gov.hmrc"      %% "domain"            % domainVersion,
     "com.google.guava" % "guava"              % "18.0"
   )
@@ -50,11 +50,10 @@ private object AppDependencies {
         override lazy val test = Seq(
           "uk.gov.hmrc"            %% "hmrctest"           % hmrcTestVersion % scope,
           "org.mockito"            % "mockito-all"         % "1.10.19",
-          "org.scalatest"          %% "scalatest"          % "2.2.2" % scope,
           "org.pegdown"            % "pegdown"             % "1.4.2" % scope,
           "com.github.tomakehurst" % "wiremock"            % "1.52" % scope,
           "com.typesafe.play"      %% "play-test"          % PlayVersion.current % scope,
-          "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.0" % "test"
+          "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % "test"
         )
       }.test
   }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,9 +5,9 @@ resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/
 
 resolvers += Resolver.bintrayRepo("hmrc", "release-candidates")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.4.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.8.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "0.9.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "0.10.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "1.0.0")
 
