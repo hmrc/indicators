b4d9b4625ce56c33c202bb9873358c2af450d2bd
Jakob Grunig <jakob.grunig@digital.hmrc.gov.uk>
2018-08-07 16:10:57 +0100

    PLATOPS-1514 - updated dependencies

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -14,7 +14,7 @@ object MicroServiceBuild extends Build with MicroService {
     SbtDistributablesPlugin
   )
 
-  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
+  override lazy val appDependencies: Seq[ModuleID] = AppDependencies.compile ++ AppDependencies.test
 }
 
 private object AppDependencies {
@@ -22,41 +22,23 @@ private object AppDependencies {
   import play.core.PlayVersion
   import play.sbt.PlayImport._
 
-  private val microserviceBootstrapVersion = "6.7.0"
-  private val playUrlBindersVersion        = "2.1.0"
-  private val domainVersion                = "5.1.0"
-  private val hmrcTestVersion              = "3.0.0"
-
   resolvers += "Sonatype OSS Snapshots" at "http://jcenter.bintray.com"
 
   val compile = Seq(
-    "uk.gov.hmrc"      %% "bootstrap-play-25" % "1.5.0",
-    "uk.gov.hmrc"      %% "play-url-binders"  % playUrlBindersVersion,
-    "uk.gov.hmrc"      %% "domain"            % domainVersion,
+    "uk.gov.hmrc"      %% "bootstrap-play-25" % "1.7.0",
+    "uk.gov.hmrc"      %% "play-url-binders"  % "2.1.0",
     "uk.gov.hmrc"      %% "git-client"        % "0.6.0",
     "uk.gov.hmrc"      %% "github-client"     % "1.21.0",
-    "uk.gov.hmrc"      %% "domain"            % domainVersion,
+    "uk.gov.hmrc"      %% "domain"            % "5.2.0",
     "com.google.guava" % "guava"              % "18.0"
   )
 
-  trait TestDependencies {
-    lazy val scope: String       = "test"
-    lazy val test: Seq[ModuleID] = ???
-  }
-
-  object Test {
-    def apply() =
-      new TestDependencies {
-        override lazy val test = Seq(
-          "uk.gov.hmrc"            %% "hmrctest"           % hmrcTestVersion % scope,
-          "org.mockito"            % "mockito-all"         % "1.10.19",
-          "org.pegdown"            % "pegdown"             % "1.4.2" % scope,
-          "com.github.tomakehurst" % "wiremock"            % "1.52" % scope,
-          "com.typesafe.play"      %% "play-test"          % PlayVersion.current % scope,
-          "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % "test"
-        )
-      }.test
-  }
-
-  def apply() = compile ++ Test()
+  val test = Seq(
+    "uk.gov.hmrc"            %% "hmrctest"           % "3.0.0"   % "test",
+    "org.mockito"            % "mockito-all"         % "1.10.19" % "test",
+    "org.pegdown"            % "pegdown"             % "1.4.2"   % "test",
+    "com.github.tomakehurst" % "wiremock"            % "1.52"    % "test",
+    "com.typesafe.play"      %% "play-test"          % PlayVersion.current % "test",
+    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1"   % "test"
+  )
 }
